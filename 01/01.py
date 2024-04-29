7e8ae26a3b56c8cadc0040d10165004366996204
miyachun <miyachun808@gmail.com>
2024-04-29 08:10:10 +0800

    init

--- 01/01.py
+++ 01/01.py
@@ -1,8 +1,8 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"D:\path\Tesseract-OCR\tesseract.exe"
-img = Image.open("english.jpg")
+#pytesseract.pytesseract.tesseract_cmd = r"D:\Chung_Hua_University\2024\Tesseract-OCR\tesseract.exe"
+img = Image.open(r"word.jpg")
 #img.show()
 print(pytesseract.image_to_string(img, lang="eng"))
 
--- 01/04.py
+++ 01/04.py
@@ -1,40 +1,8 @@
+from PIL import Image
+import pytesseract
 import numpy as np
-import cv2
 
-img = np.zeros((400, 400, 3), np.uint8)
-img.fill(90)
-
-
-text = 'Hello, OpenCV!'
-
-
-cv2.putText(img, text, (10, 40), cv2.FONT_HERSHEY_SIMPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 80), cv2.FONT_HERSHEY_PLAIN,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 120), cv2.FONT_HERSHEY_DUPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 160), cv2.FONT_HERSHEY_COMPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 200), cv2.FONT_HERSHEY_TRIPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 240), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 280), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.putText(img, text, (10, 320), cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
-  1, (0, 255, 255), 1, cv2.LINE_AA)
-
-cv2.imshow('My Image', img)
-cv2.imwrite('my.jpg', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+filename = '2_python-ocr.jpg'
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+print(text)
